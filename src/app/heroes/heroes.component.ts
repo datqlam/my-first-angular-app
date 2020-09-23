e8d94f6acf39b78d721844d376c49cb9f419be6a
Dat Lam <lqdat2n@gmail.com>
2020-09-23 13:15:22 -0400

    Get data from in-memory server

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -27,4 +27,20 @@ export class HeroesComponent implements OnInit {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 
+  // When the given name is non-blank, the handler creates a Hero-like object from the name
+  // (it's only missing the id) and passes it to the services addHero() method.
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
+
 }
